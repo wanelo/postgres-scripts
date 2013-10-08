4b19a1146d1d0b7302f3d8c15f4b7e6d63735abe
Eric Saxby <sax@livinginthepast.org>
2013-10-08 15:43:06 -0700

    Add error handling to replicate script
    Make script more verbose and check for errors on
    important actions.

--- replicate.sh
+++ replicate.sh
@@ -19,15 +19,26 @@ MASTER_IP=$1
 DATA_DIR=${2:-$DEFAULT_DATA_DIR}
 SERVICE_NAME=$(svcs -a | grep postgres | awk '{ print $3 }')
 
+echo "Using -- data directory: $DATA_DIR"
+echo "           service name: $SERVICE_NAME"
+echo "           ip of master: $MASTER_IP"
+echo
+
 # Ensure services don't interrupt us
 if [ ! -z $SERVICE_NAME ]; then
+  echo "Disabling service $SERVICE_NAME..."
   svcadm disable -s $SERVICE_NAME
 fi
+echo "Disabling chef-client..."
 svcadm disable -s chef-client
 
 # Clean up previous attempts
-rm -rf $DATA_DIR.original
-mv $DATA_DIR $DATA_DIR.original
+rm -rf $DATA_DIR.orig
+test -d $DATA_DIR
+if [ $? -eq 0 ]; then
+  echo "Moving $DATA_DIR to $DATA_DIR.orig..."
+  mv $DATA_DIR $DATA_DIR.orig
+fi
 
 # Tune TCP settings
 ndd -set /dev/tcp tcp_max_buf 2097152
@@ -36,14 +47,24 @@ ndd -set /dev/tcp tcp_recv_hiwat 2097152
 ndd -set /dev/tcp tcp_xmit_hiwat 2097152
 
 # Run base backup
-if [[ "$DATA_DIR" =~ "93" ]]; then
-  pg_basebackup -X stream -D $DATA_DIR -P -U postgres -h $MASTER_IP
-else
+if [[ "$DATA_DIR" =~ "92" ]]; then
+  echo "Starting basebackup, using -X fetch"
   pg_basebackup -X fetch -D $DATA_DIR -P -U postgres -h $MASTER_IP
+else
+  echo "Starting basebackup, using -X stream (supported by PostgreSQL 9.3+)"
+  pg_basebackup -X stream -D $DATA_DIR -P -U postgres -h $MASTER_IP
 fi
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: basebackup failed" >&2
+  exit 1
+fi
+
+echo "Chowning $DATA_DIR"
 chown -R postgres:postgres $DATA_DIR
 
 # Configure recovery.conf file
+echo "Configuring replica: $DATA_DIR/recovery.conf"
 cat > $DATA_DIR/recovery.conf <<DELIM
 standby_mode = 'on'
 primary_conninfo = 'host=$MASTER_IP'
@@ -57,4 +78,12 @@ DELIM
 chown postgres:postgres $DATA_DIR/recovery.conf
 
 # Update Postgres settings (local IP) and start it
-chef-client
+echo "Running chef to update configuration"
+chef-client > /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "ERROR: chef-client failed" >&2
+  exit 1
+fi
+
+echo "Done"
